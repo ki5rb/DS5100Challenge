afdd04bcdc063d9f705ab204d342bcf2ee719459
ki5rb@virginia.edu <ki5rb@virginia.edu>
2024-03-28 21:14:39 -0400

    setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 from setuptools import setup
+setup(
+    name='Demo',
+    version='0.1',
+    description = "a simple demosntration package",
+    url = 'http://github.com/ki5rb/DS5100Challenge',
+    author = 'Karunya Iyappan',
+    author_email = "ki5rb@virginia.edu",
+    license = 'MIT',
+    packages = ['demo']
+)
